6b2b929e965ebc19bc84d5a2153d960b5b028495
Collinscodesproper <mwendacollinsgakii@gmail.com>
2024-03-09 11:50:25 +0300

    First commit

---
+++ week2.py
@@ -0,0 +1,13 @@
+my_list = []
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+my_list.insert(1,15)
+list2=[50,60,70]
+my_list+=list2
+my_list.pop(-1)
+my_list.sort
+value=30
+index=my_list.index(value)
+print("Index of", value, " is", index)
